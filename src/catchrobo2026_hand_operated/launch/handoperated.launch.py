948985358e2f759a8935c07186d24835852b6bdf
grafen1202 <grafen1202>
2026-08-31 00:52:05 +0900

    launch fix

--- src/catchrobo2026_hand_operated/launch/handoperated.launch.py
+++ src/catchrobo2026_hand_operated/launch/handoperated.launch.py
@@ -4,12 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # 1. コントローラーのハードウェア入力を読み取るROS 2標準ノード
-        Node(
-            package='joy',
-            executable='joy_node',
-            name='joy_node',
-            output='screen'
-        ),
+        # Node(
+        #     package='joy',
+        #     executable='joy_node',
+        #     name='joy_node',
+        #     output='screen'
+        # ),
         
         # 2. 逆運動学のサービスサーバーノード
         Node(
